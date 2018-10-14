3e534dfdc0b291f3c2012ffd4ce7802062d626bc
Mohammed Shafiuddin <37742815+mshafi2@users.noreply.github.com>
2018-10-13 23:12:28 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -30,6 +30,21 @@ int main(void){
   for (;j<i;j++){
     printf("argsarray[%d]: %s\n", j, argsarray[j]);
 }
+ //-----------------------------------------------------
+    int pid = fork();
+  if (pid == 0) {
+    printf("I am the child!  I have pid %d.\n", getpid());
+    exit(6);
+  } else {
+    printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
+    int status;
+    wait(&status);
+    printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
+  }
+    
+    
+ //-----------------------------------------------------
+    
         int fd;
         int ret;
 
