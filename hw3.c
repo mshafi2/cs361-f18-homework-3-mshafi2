f3e3a850942ad8dd2454b8307e71841320ae4969
Mohammed Shafiuddin <37742815+mshafi2@users.noreply.github.com>
2018-10-13 23:01:15 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 int main(){
 
@@ -29,5 +33,29 @@ int main(){
   for (;j<i;j++){
     printf("argsarray[%d]: %s\n", j, argsarray[j]);
   }  
+   int fd;
+        int ret;
+
+        fd = open("output.txt", O_CREAT | O_APPEND | O_WRONLY);
+
+        if(fd < 0)
+        {
+          perror("open");
+           exit(1);
+         }
+
+        ret = dup2(fd, 1);
+
+        if(ret < 0)
+        {
+           perror("dup2");
+           exit(1);
+        }
+
+        system("ls /tmp");
+        close(fd);
+
+        return 0;
+
 
 }
