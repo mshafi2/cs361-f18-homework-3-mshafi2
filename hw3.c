a707341e4d7be4bd76dba77d99b18f0b3a241b75
Mohammed Shafiuddin <37742815+mshafi2@users.noreply.github.com>
2018-10-16 23:49:12 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -4,46 +4,45 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
-
+#include <ctype.h>
 
 int main(){
   char line[500];
   char argsarray[20][100];
 
   char **malArray = (char **) malloc(1000 * sizeof(char));
+  char **malArray1 = (char **) malloc(1000 * sizeof(char));
   
- 
+  
 while(1){
  printf("CS361 > ");
  
- fgets(line, 500, stdin);
+ char *input;
+ fgets(line, 500 , stdin);
  
- 	line[strlen(line) - 1] = '\0';
+ line[strlen(line) - 1] = '\0';
  	
- if(strcmp(line, "exit") ==0){
+ if(strcmp(line, "exit") ==0)
+ {
  	exit(0);
  }
  
-
-  char *word = strtok(line, " ");
-  int i = 0;
-   
+  char *delimiter = " " " ; ";
+  char *word = strtok(line, delimiter);
   
-  while (word) 
+   int i = 0;
+while (word) 
   {
-  	
-    printf("word: %s\n", word);
-    
-    //copy a word to the arg array
+  	  //copy a word to the arg array
     strcpy(argsarray[i], word);
     
+
     //get next word
     word = strtok(NULL, " ");
     
-    
     malArray[i] = argsarray[i];
     i = i + 1;
-    
+
     //printf("pid:%d status:%d\n", getpid(), pid);
   }
     int pid = fork();
@@ -60,9 +59,13 @@ while(1){
     wait(&status);
    // printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
   }
-   
-   
-  }
+//   int j=0; 
+//  for (;j<i;j++){
+//    printf("argsarray[%d]: %s\n", j, argsarray[j]);
+//  }  
+//   
+//--------------------------------------------------
 
 		return 0;
+}
 }
