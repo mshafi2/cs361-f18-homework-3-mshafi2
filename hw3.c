3f29102873e7d4e2b74e5694e27e2d506d56d419
Mohammed Shafiuddin <37742815+mshafi2@users.noreply.github.com>
2018-10-18 12:46:37 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -9,45 +9,77 @@
 int main(){
   char line[500];
   char argsarray[20][100];
+  char argsarray1[20][100];
 
-  char **malArray = (char **) malloc(1000 * sizeof(char));
-  
- 
-while(1){
- printf("CS361 > ");
- 
- fgets(line, 500, stdin);
- 
- 	line[strlen(line) - 1] = '\0';
+  char **malArray = (char **) malloc(30 * sizeof(char));
+  char **malArray1 = (char **) malloc(30 * sizeof(char));
  	
- if(strcmp(line, "exit") ==0){
- 	exit(0);
- }
  
-
-  char *word = strtok(line, " ");
-  int i = 0;
-   
+	while(1)
+	{
+ 		printf("CS361 > ");
+ 
+ 		fgets(line, 500, stdin);
+ 
+ 		line[strlen(line) - 1] = '\0';
+ 	
+ 		if(strcmp(line, "exit") ==0)
+ 		  {
+ 				exit(0);
+ 		  }
+ 
+ 	   	char *word = strtok(line, " ");
+  	   	int i = 0;
+  	   	int u = 0;
   
-  while (word) 
-  {
-  	
-    printf("word: %s\n", word);
-    
-    //copy a word to the arg array
-    strcpy(argsarray[i], word);
+		while (word) 
+		{
+		    printf("word1: %s\n", word);
+		    int ret;
+		    ret = strcmp(word, ";");
+		  	if(ret == 0)
+		  	{
+		  		word =strtok(NULL, " ");
+		  		
+	  		
+		   			while(word)
+		    	 	{
+			   			printf("word2: %s\n", word);
+			   			strcpy(argsarray1[u], word);
+			   		 		//get next word
+			    		word = strtok(NULL, " ");
+			    		malArray1[u] = argsarray1[u];
+			   				 u = u + 1;
+			   			malArray1 [u] = NULL;
+					}
+				break;	
+			}
+//------------------------------------------------------------
+   		 //copy a word to the arg array
+    	 strcpy(argsarray[i], word);
+    	 //get next word
+    	 word = strtok(NULL, " ");
+     	 malArray[i] = argsarray[i];
+      	 	i = i + 1;
+      	 	malArray [i] = NULL;
+  		}
+  		
+  		
+  		  //print out our array
+  int j=0;
    
-	
-    //get next word
-    word = strtok(NULL, " ");
-    
-    
-    malArray[i] = argsarray[i];
-    i = i + 1;
-    
-    //printf("pid:%d status:%d\n", getpid(), pid);
-  }
-    int pid = fork();
+  for (;j<i;j++){
+    printf("argsarray[%d]: %s\n", j, argsarray[j]);
+  }  
+  
+    //print out our array
+  
+  for (j=0; j<u;j++){
+    printf("argsarray1[%d]: %s\n", j, argsarray1[j]);
+  }  
+  		
+  		
+  		 int pid = fork();
   if (pid == 0) 
   {
     printf("pid:%d status:%d\n", getpid());
@@ -61,9 +93,26 @@ while(1){
     wait(&status);
    // printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
   }
-   
-   
+  
+  int pid1 = fork();
+  if (pid1 == 0) 
+  {
+    printf("pid:%d status:%d\n", getpid());
+    execv(malArray1[0], malArray1);
+   // printf("Hello adding print statement\n");
+  } 
+  else 
+  {
+   // printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
+    int status1;
+    wait(&status1);
+   // printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
   }
+}
+   
+   
+   
+  
 
 		return 0;
 }
