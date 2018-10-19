8d53df5231f6fac6b4352113b5770a0089d456ea
Mohammed Shafiuddin <37742815+mshafi2@users.noreply.github.com>
2018-10-18 21:47:26 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -124,9 +124,9 @@ int main(){
   {
   	
    // printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
-    int status1;
+    int status1; 
     wait(&status1);
-    printf("pid:%d status:%d\n", pid(), WEXITSTATUS(status1));
+    printf("pid:%d status:%d\n", pid1(), WEXITSTATUS(status1));
    // printf("My child has died with status %d. :(\n", WEXITSTATUS(status));
   }
 }
