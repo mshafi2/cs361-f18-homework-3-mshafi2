3c81dfb5fc67db6fe12ca471a776ef98119307e6
Mohammed Shafiuddin <37742815+mshafi2@users.noreply.github.com>
2018-10-18 20:35:30 -0500

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -7,9 +7,10 @@
 #include <sys/types.h>
 //---------------------------------------
 void sigint_handler(int sig){
-  char msg[] = "Signal handled.  Deal with it.\n";
-  write(1, msg, sizeof(msg));
-  exit(0);
+ // char msg[] = "Signal handled.  Deal with it.\n";
+ // write(1, msg, sizeof(msg));
+ (void)signal(SIGINT, SIG_DFL);
+  //exit(0);
 }
 
 //----------------------------------------
@@ -121,46 +122,15 @@ int main(){
   }
 }
 //===========================IOREDIRECT======================================
-char buf1[20];
-  char buf2[20];
-  char buf3[20];
-  char buf4[20];
-  
-  int fd1 = open("sampletext1.txt", O_RDONLY);
-  int fd2 = open("sampletext2.txt", O_RDONLY);
 
-int fd = open("output.txt", O_RDWR);
-dup2(fd,2);
-fprintf(stderr,"Hello world\n");
-
-  read(fd1, buf1, 19);
-  buf1[19] = '\0';
-  printf("%s\n", buf1);
-
-  read(fd2, buf2, 19);
-  buf2[19] = '\0';
-  printf("%s\n", buf2);
-
-  dup2(fd1, fd2);
-
-  read(fd1, buf3, 19);
-  buf3[19] = '\0';
-  printf("%s\n", buf3);
-
-  read(fd2, buf4, 19);
-  buf4[19] = '\0';
-  printf("%s\n", buf4);
-  
 //============================SIGNAL HANDLER=====================================
   signal(SIGINT, sigint_handler);
 
   int i = 99;
   while(1){
-    printf("%d bottles of beer on the wall\n", i);
-    printf("%d bottles of beer\n", i);
-    printf("Take one down, pass it around\n");
-    i = i - 1;
-    printf("%d bottles of beer on the wall\n", i);
+    sleep(0);
+    /i = i - 1;
+    
   } 
   
   
